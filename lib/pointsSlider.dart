24b21082acca16bd0530458a737e360e0814ad76
icsdaniel <daniele96carta@gmail.com>
2021-08-17 20:52:34 +0200

    Slider covered using opacity container

--- lib/pointsSlider.dart
+++ lib/pointsSlider.dart
@@ -10,13 +10,19 @@ class PointsSlider extends StatefulWidget {
 
 class _PointsSliderState extends State<PointsSlider> {
   var rng = new Random();
-  double bottomPlacement = 46;
-  final double blockHeight = 30;
+  double bottomPlacement = 560;
+  final double blockHeight = 20;
   final double barHeight = 660;
   final double width = 80;
   int points = 0;
-  double value = 75;
+  double value = 0;
+  double toShow=1.0;
 
+  void _showScore() {
+    setState(() {
+      toShow=0;
+    });
+  }
   void _refreshBottomPlacement() {
     setState(() {
       //bottomPlacement = rng.nextDouble() * (barHeight - blockHeight);
@@ -48,96 +54,104 @@ class _PointsSliderState extends State<PointsSlider> {
     //positioning using a relative distance from the top or the bottom
     //we first place the max points block using random positioning in the stack and then build the
     //rest of the point bar working from it.
-    return Container(
-      color: Colors.black,
-      height: barHeight,
-      width: width,
-      child: SliderTheme(
-        data: SliderThemeData(
-          trackHeight: width, //larghezza
-          thumbShape: SliderComponentShape
-              .noOverlay, //doesn't show the dot that's draggable
-          overlayShape:
-              SliderComponentShape.noOverlay, //its shade doesn't show up either
-          valueIndicatorShape:
-              SliderComponentShape.noOverlay, //no value tag when dragged
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+      Container(
+        color: Colors.black,
+        height: barHeight,
+        width: width,
+        child: SliderTheme(
+          data: SliderThemeData(
+            trackHeight: width, //larghezza
+            thumbShape: SliderComponentShape
+                .noOverlay, //doesn't show the dot that's draggable
+            overlayShape: SliderComponentShape
+                .noOverlay, //its shade doesn't show up either
+            valueIndicatorShape:
+                SliderComponentShape.noOverlay, //no value tag when dragged
 
-          trackShape: RectangularSliderTrackShape(),
-        ),
-        child: Stack(
-          children: [
-            Positioned(
-              bottom: bottomPlacement,
-              child: Container(
-                child: Text("5", textAlign: TextAlign.center),
-                width: width,
-                height: blockHeight,
-                color: Colors.red,
-              ),
-            ),
-            Positioned(
-              bottom: bottomPlacement + blockHeight,
-              child: Container(
-                child: Text("3", textAlign: TextAlign.center),
-                width: width,
-                height: blockHeight,
-                color: Colors.orange,
-              ),
-            ),
-            Positioned(
-              bottom: bottomPlacement - blockHeight,
-              child: Container(
-                child: Text("3", textAlign: TextAlign.center),
-                width: width,
-                height: blockHeight,
-                color: Colors.orange,
-              ),
-            ),
-            Positioned(
-              bottom: bottomPlacement + 2 * blockHeight,
-              child: Container(
-                child: Text("1", textAlign: TextAlign.center),
-                width: width,
-                height: blockHeight,
-                color: Colors.yellow,
-              ),
-            ),
-            Positioned(
-              bottom: bottomPlacement - 2 * blockHeight,
-              child: Container(
-                child: Text("1", textAlign: TextAlign.center),
-                width: width,
-                height: blockHeight,
-                color: Colors.yellow,
-              ),
-            ),
-            Opacity(
-              opacity: 0.6,
-              child: RotatedBox(
-                quarterTurns: 3, //to rotate the slider
-                child: Slider(
-                  value: value,
-                  min: 0,
-                  max: barHeight,
-                  divisions: barHeight.toInt(),
-                  label: value.round().toString(),
-                  onChanged: (value) => setState(() => this.value = value),
+            trackShape: RectangularSliderTrackShape(),
+          ),
+          child: Stack(
+            children: [
+              Positioned(
+                bottom: bottomPlacement,
+                child: Container(
+                  child: Text("5", textAlign: TextAlign.center),
+                  width: width,
+                  height: blockHeight,
+                  color: Colors.red,
                 ),
               ),
-            ),
-            Center(
-              child: Text(
-                '${value.round()}\n${points}',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
+              Positioned(
+                bottom: bottomPlacement + blockHeight,
+                child: Container(
+                  child: Text("3", textAlign: TextAlign.center),
+                  width: width,
+                  height: blockHeight,
+                  color: Colors.orange,
                 ),
               ),
-            )
-          ],
+              Positioned(
+                bottom: bottomPlacement - blockHeight,
+                child: Container(
+                  child: Text("3", textAlign: TextAlign.center),
+                  width: width,
+                  height: blockHeight,
+                  color: Colors.orange,
+                ),
+              ),
+              Positioned(
+                bottom: bottomPlacement + 2 * blockHeight,
+                child: Container(
+                  child: Text("1", textAlign: TextAlign.center),
+                  width: width,
+                  height: blockHeight,
+                  color: Colors.yellow,
+                ),
+              ),
+              Positioned(
+                bottom: bottomPlacement - 2 * blockHeight,
+                child: Container(
+                  child: Text("1", textAlign: TextAlign.center),
+                  width: width,
+                  height: blockHeight,
+                  color: Colors.yellow,
+                ),
+              ),
+              Opacity(
+                  opacity: toShow,
+                  child: Expanded(child: Container(color: Colors.green))),
+              Opacity(
+                opacity: 0.6,
+                child: RotatedBox(
+                  quarterTurns: 3, //to rotate the slider
+                  child: Slider(
+                    value: value,
+                    min: 0,
+                    max: barHeight,
+                    divisions: barHeight.toInt(),
+                    label: value.round().toString(),
+                    onChanged: (value) => setState(() => this.value = value),
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  '${value.round()}\n${points}',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-    );
+      CloseButton(onPressed: _showScore)
+    ]);
   }
 }
